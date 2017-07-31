abaf872c56fb7d38985202ea43f648380f0b7d5c
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-07-31 08:25:54 +0000

    echo.sh edited online with Bitbucket

--- echo.sh
+++ echo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-echo "sample pipeline in production and gitRepo as well as pipeline jobs are in same repositories!"
+echo "sample pipeline in production and gitRepo as well as pipeline jobs are in same repositories!!!"
